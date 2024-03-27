a5c625f0912c92ea1dc3a90e4cf867672edd9221
Tamerbekir <astoryofame@gmail.com>
2024-03-26 21:40:29 -0400

    fixed syntax in homeRoutes. Added additional redirects. Added HTML to main handlebars for login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,16 +9,28 @@ const withAuth = require('../utils/auth')
 
 
 router.get('/', withAuth, (req, res) => {
-    res.render('homepage', { loggedIn: req.session.loggedIn });
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+    } else {
+        res.render('login');
+    }
 });
 
 //renders the login page to the user
 // if user is logged in they will go to their dashboard, otherwise they see the login page again
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/homepage');
+        res.redirect('/dashboard');
     } else {
-        res.render('/login');
+        res.render('login'); 
+    }
+});
+
+router.get('/dashboard', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+    } else {
+        res.render('login'); 
     }
 });
 
